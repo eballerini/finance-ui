5047b60d43ba396180fe4451f98fb317d3c788b0
Emmanuel B <emmanuel.ballerini@gmail.com>
2020-04-08 19:28:11 -0400

    make an API call to get the transactions

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
 import TransactionList from './transactions.js';
 
 function App() {
-  const transactions = get_transactions();
+  const [transactions, setTransactions] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  
+  // TODO move this to a function
+  useEffect(() => {
+    fetch(
+      `http://localhost:8080/expense/accounts/1/transactions`,
+      {
+        method: "GET",
+        headers: new Headers({
+          Accept: "application/vnd.github.cloak-preview"
+        })
+      }
+    )
+      .then(res => res.json())
+      .then(response => {
+        setTransactions(response);
+        setIsLoading(false);
+      })
+      .catch(error => console.log(error));
+  }, []);
+  
   return (
     <div className="App">
+      {isLoading && <p>Loading transactions</p>}
       <div>
         <TransactionList transactions={transactions}/>
       </div>
     </div>
   );
-}
-
-function get_transactions() {
-  // TODO make a network request
-  return [{
-    'key': 1,
-    'description': 'lunch'
-  }, {
-    'key': 2,
-    'description': 'dinner'
-  }];
 }
 
 export default App;
--- src/transactions.js
+++ src/transactions.js
@@ -1,23 +1,19 @@
 import React from 'react';
 
-class TransactionList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+function TransactionList(props) {
   
-  render() {
-    const transactionList = this.props.transactions.map((tx, index) => 
-      <li key={index}>Desc: {tx.description}</li>
-    );
-    return ( 
+  const transactionList = props.transactions.map((tx, index) => 
+    <li key={index}>Desc: {tx.description}</li>
+  );
+  
+  return (
+    <div>
+      <h1>Welcome to the transactions page</h1>
       <div>
-        <h1>Welcome to the transactions page</h1>
-        <div>
-          <ul>{transactionList}</ul>
-        </div>
+        <ul>{transactionList}</ul>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default TransactionList;
